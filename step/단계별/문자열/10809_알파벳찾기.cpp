bc872b19b9ac61fa41e4a1d67f687c99f171b096
minsu kyung <ghtn9898@gmail.com>
2022-01-07 20:58:42 +0900

    boj

---
+++ step/단계별/문자열/10809_알파벳찾기.cpp
@@ -0,0 +1,19 @@
+/* 알파벳 소문자로만 이루어진 단어 S가 주어진다. 
+각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, alp[26];
+    fill_n(alp, 26, -1);        //배열을 -1로 초기화
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.length();i++) {
+        a = int(s[i]) - 97;
+        if (alp[a] == -1) alp[a] = i;
+    }
+    for (int j = 0;j < sizeof(alp)/sizeof(int);j++) {
+        cout << alp[j]<< " ";
+    }   
+}
