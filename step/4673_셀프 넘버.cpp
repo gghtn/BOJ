7dda9a66dd3d190458e5be1f13891490cf45d105
minsu kyung <ghtn9898@gmail.com>
2022-01-07 16:30:59 +0900

    boj

--- step/4673_셀프 넘버.cpp
+++ step/4673_셀프 넘버.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void selfN(int a){
-    int arr[10001] = {0,},n;
-    for(int i=a; i<10000;i++){
-        if(arr[i] != 1) cout<< i <<endl;
-        n = i + i/1000 + (i%1000)/100 + (i%100)/10 + i%10;
-        if(n <= 10000) arr[n] = 1;
+int selfN(int a){
+    int n = a;
+    for(;a != 0; a /= 10){
+        n += a%10;
     }
+    return n;
 }
 
 int main(){
-    selfN(1);
+    int arr[10001] = {0,};
+    for(int i =1; i<10000;i++){
+        if(arr[i] = 0) cout << i << endl;
+        arr[selfN(i)] = 1;
+    }
     return 0;
 }
-
