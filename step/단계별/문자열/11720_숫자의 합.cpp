cc70a13c086b55a7c34d811db0470665911eddd0
minsu kyung <ghtn9898@gmail.com>
2022-01-07 20:31:51 +0900

    boj

---
+++ step/단계별/문자열/11720_숫자의 합.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, sum=0;
+    string s;
+    cin >> N >> s;
+    for (int i = 0; i < N; i++) {
+        sum += int(s[i]) - 48;
+    }
+    cout << sum;
+}
