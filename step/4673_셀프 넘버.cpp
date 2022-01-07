83c1c1130213cc955f9b652ba484012ae6e5b156
minsu kyung <ghtn9898@gmail.com>
2022-01-07 16:23:18 +0900

    first

--- step/4673_셀프 넘버.cpp
+++ step/4673_셀프 넘버.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 void selfN(int a){
-    
+    int arr[10001] = {0,},n;
+    for(int i=a; i<10000;i++){
+        if(arr[i] != 1) cout<< i <<endl;
+        n = i + i/1000 + (i%1000)/100 + (i%100)/10 + i%10;
+        if(n <= 10000) arr[n] = 1;
+    }
 }
 
 int main(){
     selfN(1);
     return 0;
 }
+
