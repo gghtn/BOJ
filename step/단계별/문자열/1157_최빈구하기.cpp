831c81d0472b6512d8241780f1a261567a800c0c
minsu kyung <ghtn9898@gmail.com>
2022-01-08 21:04:06 +0900

    boj

---
+++ step/단계별/문자열/1157_최빈구하기.cpp
@@ -0,0 +1,10 @@
+/*주어진 단어에서 가장 많이 사용된 알파벳을 출력, 여럿 존재할 경우 ?출력*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int alphabet[26]={};
+    string alp ="abcdefghijklmnopqrstuvwxyz";
+    string word;
+
+}
