a79f497796e97d1702c02d94b95b1ddffdfce793
minsu kyung <ghtn9898@gmail.com>
2022-01-13 23:19:08 +0900

    boj

---
+++ step/단계별/기본 수학1/2292_벌집.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, prev = 1, count = 1;
+    cin >> N;
+    int i = 1;
+    while(1){
+        if(prev < N){
+            prev += 6*i;
+            count++;
+            i++;
+        }
+        else
+            break;
+    }
+    cout << count;
+}
