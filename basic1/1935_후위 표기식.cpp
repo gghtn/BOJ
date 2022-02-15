5ed9bbbcb53e1423bc72f0a12b7270b35b816b0e
minsu kyung <ghtn9898@gmail.com>
2022-02-15 22:16:44 +0900

    boj

---
+++ basic1/1935_후위 표기식.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    int n;
+    string s;
+    double op;
+    stack<double> st;
+    cin >> n >> s;
+    cout << fixed;
+    cout.precision(2);
+
+    for(int i=0;i<n;i++){
+        cin >> op;
+    }
+}
