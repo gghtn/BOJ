695448b40fd5d4dd7bc20ae7779dc4f2881f6837
minsu kyung <ghtn9898@gmail.com>
2022-02-16 10:33:02 +0900

    boj

--- basic1/1935_후위 표기식.cpp
+++ basic1/1935_후위 표기식.cpp
@@ -2,16 +2,45 @@
 #include<stack>
 using namespace std;
 
-int main(){
+int main() {
     int n;
     string s;
-    double op;
+    double op[26];
     stack<double> st;
     cin >> n >> s;
+
+    for (int i = 0;i < n;i++) {
+        cin >> op[i];
+    }
+
+    for (int i = 0;i < s.length();i++) {
+        if ('A' <= s[i] && s[i] <= 'Z') {
+            st.push(op[s[i] - 'A']);
+        }
+        else {
+            double a, b;
+            b = st.top();
+            st.pop();
+            a = st.top();
+            st.pop();
+            switch (s[i]) {
+            case '+':
+                st.push(a + b);
+                break;
+            case '-':
+                st.push(a - b);
+                break;
+            case '*':
+                st.push(a * b);
+                break;
+            case '/':
+                st.push(a / b);
+                break;
+            }
+        }
+    }
     cout << fixed;
     cout.precision(2);
-
-    for(int i=0;i<n;i++){
-        cin >> op;
-    }
+    cout << st.top();
+    return 0;
 }
