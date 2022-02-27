ecdf7e3e4d646c047524fde1c5de8b15c8ff8a70
minsu kyung <ghtn9898@gmail.com>
2022-02-27 23:13:26 +0900

    boj

--- basic1/2089_-2진수.cpp
+++ basic1/2089_-2진수.cpp
@@ -12,17 +12,16 @@ int main() {
     }
 
     string s;
-    while (1) {
-        if (n == 1) {
+    while (n != 0) {
+        if (n % (-2) == 0){
+            s += '0';
+            n /= -2;
+        }
+        else {
             s += '1';
-            break;
+            n = (n-1) / -2;
         }
 
-        if (n % (-2) == 0) s += '0';
-        else s += '1';
-
-        if(n < 0 && n % (-2) != 0) n = n / (-2) + 1;
-        else n = n / (-2);
     }
 
     for (int i = s.length()-1; i >= 0; i--) {
