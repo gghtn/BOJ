7cd0a39a291617f2fe54b1bfc600c18b4d2e0557
gghtn <ghtn9898@gmail.com>
2022-10-11 23:41:00 +0900

    vector<pair<>>, sort

---
+++ cpp/11650.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int compare(const pair<int, int>& a, const pair<int, int>& b) {
+	if (a.first == b.first) return a.second < b.second;
+	return a.first < b.first;
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i].first >> v[i].second;
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << ' ' << v[i].second << '\n';
+	}
+
+	return 0;
+}
