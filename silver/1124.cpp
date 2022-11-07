5fa59011ddb75139cc0178982faee18456c1c5f9
gghtn <ghtn9898@gmail.com>
2022-11-07 23:51:33 +0900

    에라토스테네스의 체

--- silver/1124.cpp
+++ silver/1124.cpp
@@ -5,35 +5,36 @@ using namespace std;
 
 #define MAX 100000
 bool arr[100001];
-
-int Factorization(int num) {
-	int result = 1, n = 2;
-	while (num != n) {
-		if (num % n == 0) {
-			num /= n;
-			result++;
-			continue;
-		}
-		n++;
-	}
-	return result;
-}
+int cnt[100001];
 
 int main() {
 	int A, B, total = 0;
 	cin >> A >> B;
 
 	memset(arr, 1, 100001);
+
 	for (int i = 2; i <= MAX; i++) {
-		if (arr[i] ==  0) continue;
+		if (arr[i] == 0) continue;
 		for (int j = 2 * i; j <= MAX; j += i) {
 			arr[j] = 0;
 		}
 	}
 
+	for (int i = 2; i <= MAX; i++) {
+		if (arr[i]) {
+			for (int j = 2*i; j <= MAX; j+=i) {
+				int tmp = j;
+				while (tmp % i == 0) {
+					tmp /= i;
+					cnt[j]++;
+				}
+			}
+		}
+	}
+
 	for (int i = A; i <= B; i++) {
 		if (arr[i]) continue;
-		if (arr[Factorization(i)]) total++;
+		if (arr[cnt[i]]) total++;
 	}
 	cout << total;
 	return 0;
