119a3e180271a605d93060ca143098a2b83eef8f
minsu <ghtn9798@naver.com>
2023-01-16 22:24:05 +0900

    DP

--- DynamicProgramming/11053.cpp
+++ DynamicProgramming/11053.cpp
@@ -1,96 +1,39 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cstring>
-#include <queue>
 
 using namespace std;
 
-struct vertex
-{
-	int		start;		// 처음 점
-	int		end;		// 나중 점
-
-	void swap()
-	{
-		int tmp = 0;
-		tmp = start;
-		start = end;
-		end = tmp;
-	}
-};
-
-vector<vertex> v;
-queue<int> q;
-int N, M, V, arr[1001];
-
-bool compare(vertex& a, vertex& b)
-{
-	if (a.start == b.start) return a.end < b.end;
-	return a.start < b.start;
-}
-
-void _DFS(int num)
-{
-	if (!arr[num])
-	{
-		arr[num] = 1;
-		cout << num << ' ';
-	}
-	else return;
-
-	for (int i = 0; i < v.size(); i++)
-	{
-		if (v[i].start == num || v[i].end == num)
-		{
-			if (v[i].start == num) _DFS(v[i].end);
-			else _DFS(v[i].start);
-		}
-	}
-}
-
-void _BFS(int num)
-{
-	int temp = 0;
-	q.push(num);
-
-	while (!q.empty())
-	{
-		temp = q.front();
-		if (!arr[temp])
-		{
-			arr[temp] = 1;
-			cout << temp << ' ';
-		}
-		q.pop();
-		for (int i = 0; i < v.size(); i++)
-		{
-			if ((v[i].start == temp && !arr[v[i].end]) || (v[i].end == temp && !arr[v[i].start]))
-			{
-				if (v[i].start == temp) q.push(v[i].end);
-				else  q.push(v[i].start);
-			}
-		}
-	}
-}
+int arr[1001];		// 수열정보 저장하는 배열
+int arr_num[1001];	// index부터 뒤에 더 큰 수가 몇개 있는지 저장하는 배열
 
 int main()
 {
-	cin >> N >> M >> V;
-	v.resize(M);		// 간선의 수만큼 v의 사이즈 변경
+	int N, length = 1, temp = 0, temp2 = 0, max_num = 1;
+	cin >> N;
 
-	for (int i = 0; i < M; i++)
+	for (int i = 0; i < N; i++)
 	{
-		cin >> v[i].start >> v[i].end;
-		if (v[i].start > v[i].end) v[i].swap();	// 양방향이므로 순서는 중요하지 않기 때문에 낮은 순으로 저장
+		cin >> arr[i];
 	}
-	sort(v.begin(), v.end(), compare);
 
-	_DFS(V);
-	cout << '\n';
-	memset(arr, 0, sizeof(int) * 1001);		// 출력했는지 확인하는 배열 초기화
+	fill(arr_num, arr_num + 1001, 1);
 
-	_BFS(V);
+	for (int i = N - 2; 0 <= i; i--)
+	{
+		for (int j = i + 1; j < N; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				temp = arr_num[i] + arr_num[j];
+				temp2 = max(temp2, temp);
+			}
+		}
+		if (temp2 > 0) arr_num[i] = temp2;
+		max_num = max(max_num, arr_num[i]);
+		temp = 0, temp2 = 0;
+	}
+
+	cout << max_num;
 
 	return 0;
 }
