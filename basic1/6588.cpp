58e0f36232d0765a85b43a27d1bf1e80b257aced
minsu <ghtn9798@naver.com>
2023-02-10 16:50:37 +0900

    boj

--- basic1/6588.cpp
+++ basic1/6588.cpp
@@ -1,17 +1,28 @@
-/*
-골드바흐의 추측 - 4보다 큰 모든 짝수는 두 홀수 소수의 합으로 나타낼 수 있다.
-백만 이하의 모든 짝수에 대해서, 이 추측을 검증하는 프로그램을 작성하시오
-*/
-#include<iostream>
+#include <iostream>
 #include<cmath>
 using namespace std;
 
-bool prime(int x) {
-    if (x == 1) return false;
-    for (int i = 2; i <= sqrt(x);i++) {
-        if (x % i == 0) return false;
+#define MAX_NUM     1000001
+
+//bool prime(int x) {
+//    if (x == 1) return false;
+//    for (int i = 2; i <= sqrt(x); i++) {
+//        if (x % i == 0) return false;
+//    }
+//    return true;
+//}
+
+bool bArr[MAX_NUM];
+void Eratos()
+{
+    memset(bArr, 1, sizeof(bool) * MAX_NUM);
+    for (int i = 2; i < MAX_NUM; ++i)
+    {
+        if (bArr[i] == 0) continue;
+
+        for (int j = 2 * i; j < MAX_NUM; j += i)
+            bArr[j] = 0;
     }
-    return true;
 }
 
 int main() {
@@ -19,21 +30,25 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
+    Eratos();
     int a, b;
     while (1) {
-        int n, i=1;
+        int n, i = 1;
         cin >> n;
         if (n == 0) break;
         while (1) {
             a = 2 * i + 1;
-            b = n - a;
-            if (a > b) {
-                cout << "Goldbach's conjecture is wrong." << "\n";
-                break;
-            }
-            else if (prime(a) && prime(b)) {
-                cout << n << " = " << a << " + " << b << "\n";
-                break;
+            if (bArr[a])
+            {
+                b = n - a;
+                if (a > b) {
+                    cout << "Goldbach's conjecture is wrong." << "\n";
+                    break;
+                }
+                if (bArr[b]) {
+                    cout << n << " = " << a << " + " << b << "\n";
+                    break;
+                }
             }
             i++;
         }
